2ec739af0ddfa6dbe88df1b509ec677d6e0da248
AyaAbunajm <>
2021-04-14 11:45:15 +0000

    java edit

---
+++ App.js
@@ -0,0 +1,18 @@
+//console.log('hello user')
+//console.log('welcome here ')
+// console 
+var userName=prompt('plese enter your name');
+var userAge=prompt('plese enter your userAge');
+var kindOfMovie=prompt('are you like romantic movie');
+alert ('hello'+' '+ userName);
+if (userAge>=16){
+  alert ('welcome in my movie')
+}else{
+alert ('you are under age we sorry')}
+
+if (kindOfMovie=='yes'){
+  document.write('you will love this movie');
+  }else if(kindOfMovie=='no'){
+  document.write('maybe it is not your kind of movies ');
+ }
+
