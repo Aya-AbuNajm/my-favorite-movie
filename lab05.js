64b80d69a252f16c8ebae05107da9176e65bf513
AyaAbunajm <>
2021-04-18 17:24:28 +0000

    stars

--- lab05.js
+++ lab05.js
@@ -4,16 +4,17 @@ while (starsOfMovie !=='hazel' && starsOfMovie !=='lancaster'){
 starsOfMovie = prompt('you should choose one of hazel or lancaster ');
 }
 if (starsOfMovie === 'hazel'){
-  starPic='<img src="https://s.abcnews.com/images/Nightline/140523_ntl_woodley_1250_16x9_992.jpg">'
-  document.write('wonderful hazel'); 
+  
+    starPic='<img src="https://s.abcnews.com/images/Nightline/140523_ntl_woodley_1250_16x9_992.jpg">'
+  
 }else if (starsOfMovie === 'lancaster'){
   starPic='<img src="https://s3obitbucket.s3-accelerate.amazonaws.com/obituary_image/cropped_f8ca55db.png">'
-  document.write('Amazing Lancaster'); 
 }
 
 var imgNum=prompt('how much of imag you want to see ?');
 for (var i=0;i<imgNum;i++){
   document.write(starPic);
+}
 
 
 
